fd144689ddfb4cc03d381b1b702dcc4a2166e7a9
manikyafc <manikya@fanclash.in>
2022-11-18 04:24:33 +0530

    change module path
    Update go.mod

--- internal/prometheus.go
+++ internal/prometheus.go
@@ -29,7 +29,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
-	"go.k6.io/k6/metrics"
+	stats "go.k6.io/k6/metrics"
 )
 
 type PrometheusAdapter struct {
